ffccfb0469138f6c8974e8f70f7f5f9e331ba2a0
scott-swarthout <scott.swarthout724@gmail.com>
2014-10-03 16:28:58 +0000

    go home button

--- levels/level_4_data.js
+++ levels/level_4_data.js
@@ -3,24 +3,24 @@ var level_goalcell_position = {x:1,y:3};
 var level_tetras_list = [
   {
     'type': "T",
-    "position":{x:0,y:4},
-    "orientation":"top",
+    "position":{x:3,y:0},
+    "orientation":"right",
     
     
     
 },
 {
     'type': "Z",
-    "position":{x:3,y:1},
-    "orientation":"bottom",
+    "position":{x:3,y:3},
+    "orientation":"left",
     
     
     
 },
 {
     'type': "S",
-    "position":{x:0,y:0},
-    "orientation":"right",
+    "position":{x:1,y:0},
+    "orientation":"left",
     
     
     
@@ -31,4 +31,4 @@ var level_tetras_list = [
 
 var level_wall_position = {x:2,y:3};
 
-var level_ball_position = {x:0,y:0};
+var level_ball_position = {x:3,y:3};
