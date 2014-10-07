4b2e66b8c39f31cf7c8ff55147b3082ea18d2899
scott-swarthout <scott.swarthout724@gmail.com>
2014-10-07 08:26:24 -0400

    added levels 5 and 6

---
+++ levels/level_5_data.js
@@ -0,0 +1,34 @@
+var level_goalcell_position = {x:1,y:0};
+
+var level_tetras_list = [
+  {
+    'type': "L",
+    "position":{x:0,y:3},
+    "orientation":"left",
+    
+    
+    
+},
+{
+    'type': "Z",
+    "position":{x:3,y:1},
+    "orientation":"bottom",
+    
+    
+    
+},
+{
+    'type': "T",
+    "position":{x:0,y:0},
+    "orientation":"left",
+    
+    
+    
+}
+   
+  
+];
+
+var level_wall_position = {x:2,y:4};
+
+var level_ball_position = {x:2,y:5};
---
+++ levels/level_6_data.js
@@ -0,0 +1,33 @@
+var level_goalcell_position = {x:4,y:2};
+
+var level_tetras_list = [
+  {
+    'type': "L",
+    "position":{x:1,y:3},
+    "orientation":"bottom",
+    
+    
+    
+},
+
+{
+    'type': "Z",
+    "position":{x:2,y:0},
+    "orientation":"left",
+    
+    
+},
+{
+    'type': "T",
+    "position":{x:3,y:2},
+    "orientation":"right",
+    
+    
+}
+   
+  
+];
+
+var level_wall_position = {x:3,y:3};
+
+var level_ball_position = {x:1,y:3};
